03784af93fe5d8e89ad664848e0ab49dccb4cc59
duanzilong <81650694@qq.com>
2025-11-19 14:09:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '微问道', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '最初的轻语 最远的坚持', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '问道于天 明道于己', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.920985.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '微问道,微微道,微闻道,五不散,金融,证券,股票,基金,A股,美股,炒股,博客,静态博客,动态博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
